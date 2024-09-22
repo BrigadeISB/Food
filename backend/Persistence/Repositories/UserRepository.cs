9d16b7c99457c56a9f7db3e7de4f7d4c297c7151
artemRusachenko <artemrusachenko@gmail.com>
2024-09-22 18:57:44 +0300

    feat: add UserRepo & UserConfig

---
+++ backend/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Data;
+
+namespace Persistence.Repositories
+{
+    public class UserRepository(AppDbContext context) : IUserRepository
+    {
+        public async Task AddAsync(User? user)
+        {
+            ArgumentNullException.ThrowIfNull(user);
+            await context.Users.AddAsync(user);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<User?> GetByEmailAsync(string email)
+        {
+            ArgumentNullException.ThrowIfNull(email);
+            var user = await context.Users.FirstOrDefaultAsync(x => x.Email == email);
+            return user;
+        }
+    }
+}
